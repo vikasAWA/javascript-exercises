f2670a7553a5dc634f1b32a421c0df66ada6b307
vikasAWA <85212839+vikasAWA@users.noreply.github.com>
2024-06-20 00:35:34 +0530

    Completed 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    sum = 0;
+    if (Number.isInteger(start) && Number.isInteger(end) && start > 0 && end > 0) {
+        if (start < end) {
+            for (i = start; i <= end; i++) {
+                sum += i;
+            }
+            return sum;
+        } else {
+            for (i = end; i <= start; i++) {
+                sum += i;
+            } 
+            return sum;
+        }
+    } else {
+        return "ERROR";
+    }
+    
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
