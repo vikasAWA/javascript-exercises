d1d2c8dd1dcf483e03a683e1f6eae1284d2531a3
vikasAWA <85212839+vikasAWA@users.noreply.github.com>
2024-06-19 14:46:31 +0530

    Completed the 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    reversed = '';
+    for (let i = string.length-1; i >= 0; i--) {
+        reversed += string[i];
+    }
+    return reversed
 };
 
 // Do not edit below this line
