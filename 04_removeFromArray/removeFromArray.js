ea1660faace118406ac7b7821c7aae6e68520dbb
vikasAWA <85212839+vikasAWA@users.noreply.github.com>
2024-06-19 19:28:37 +0530

    Completed exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(anArr, ...args) {
+    for (let argument of args) {
+        let i = 0;
+        while (i < anArr.length) {
+            if (anArr[i] === argument) {
+                anArr.splice(i, 1);
+            } else {
+                i++;
+            }
+        }
+        } 
+        return anArr;
+}
+console.log(removeFromArray([1, 2, 3, 4], 3, 2))
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
