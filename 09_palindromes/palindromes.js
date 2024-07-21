97a3278dce25689fd5c4621c749a1774944456a5
vikasAWA <85212839+vikasAWA@users.noreply.github.com>
2024-07-21 12:38:39 +0530

    Completed 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let str1 = '';
+    let str2 = '';
+    for (let i = 0; i < str.length; i++) {
+        if (![",", "?", ".", "!", " "].includes(str[i])) {
+            str1 += str[i];
+        }
+    }
+    for (let i = str.length-1; i >= 0; i--) {
+        if (![",", "?", ".", "!", " "].includes(str[i])) {
+            str2 += str[i];
+        }
+    }
+    if (str1.toLowerCase() === str2.toLowerCase()) {
+        return true;
+    } 
+    return false
 };
 
 // Do not edit below this line
