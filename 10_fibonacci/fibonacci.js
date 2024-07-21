3ef760c14069b17dbe1a6dbb7205e04db152acbc
vikasAWA <85212839+vikasAWA@users.noreply.github.com>
2024-07-21 13:27:22 +0530

    Completed 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    if (position < 0) {
+        return "OOPS";
+    }
+    let fibArray = [0, 1]; 
+    for (let i = 2; i <= position; i++) {
+        fibArray[i] = fibArray[fibArray.length-1] + fibArray[fibArray.length - 2];
+    }
+    return fibArray[position];
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
