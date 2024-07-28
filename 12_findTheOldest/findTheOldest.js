671ac504220e044ec5caf9903fc0cfdb0ad57411
vikasAWA <85212839+vikasAWA@users.noreply.github.com>
2024-07-28 12:58:25 +0530

    Completed 12_findOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+function getTotalAge(death, birth) {
+  if (!death) {
+    const date = new Date();
+    return date.getFullYear() - birth;
+  }
+  return death - birth;
+}
 
-};
+const findTheOldest = function(array) {
+  return array.reduce((oldest, currentItem) => {
+    const oldestAge = getTotalAge(oldest.yearOfDeath, oldest.yearOfBirth);
+    const currentAge = getTotalAge(
+      currentItem.yearOfDeath, currentItem.yearOfBirth
+    );
+    console.log(oldest);
+    return oldestAge < currentAge ? currentItem : oldest;
+  });
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
